9f388e67765d9a6d62a4169ca10586859dd41b85
namredoc <102049850+namredoc@users.noreply.github.com>
2022-08-07 16:10:30 +0300

    Autograder: Exercise 3.1

---
+++ EX_3.1.py
@@ -0,0 +1,10 @@
+hours = float(input("Enter Hours:"))
+rate = float(input("Enter Rate:"))
+standart_pay = 40 * rate
+if hours <= (40):
+    print(standart_pay)
+
+if hours > (40):
+    overtime_h = hours - 40
+    overtime_r = (rate * 1.5) * overtime_h
+    print(overtime_r + standart_pay)
