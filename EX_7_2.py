9de741cf3202e58fd8d2fa70d8dbdd8e7e986c2f
namredoc <102049850+namredoc@users.noreply.github.com>
2022-08-22 01:26:08 +0300

    Exercise 7.2 improved
    After entering incorrect filename program asks to enter a filename again.
    "quit" stops the program

--- EX_7_2.py
+++ EX_7_2.py
@@ -1,25 +1,28 @@
-# Use the file name mbox-short.txt as the file name
 
-# fname = input("Enter file name: ")
-count = 0
-avg = 0.0
-try:
-    fname = input("Enter file name: ")
-    fh = open(fname)
-except:
-    print('File',fname,'cannot be opened.')
-    exit()
 
-for line in fh:
-    if not line.startswith("X-DSPAM-Confidence:"):
-        continue
-    else:
-        count = count + 1
-        d_dots = line.find(':')
-        v_string = line[d_dots +1:]
-        value = float(v_string.strip())
-        avg = avg + value
-        # print(value, count, avg/count)   
 
-    # print(line)
-print("Average spam confidence:", avg/count)
+while True:
+        try:
+            count = 0
+            avg = 0.0
+            fname = input("Enter file name: ")
+            if fname == 'quit':
+                print("See you later")
+                break
+            fh = open(fname)
+        except:
+            print('File',fname,'cannot be opened.')
+            continue
+
+
+        for line in fh:
+            if not line.startswith("X-DSPAM-Confidence:"):
+                continue
+            else:
+                count = count + 1
+                d_dots = line.find(':')
+                v_string = line[d_dots +1:]
+                value = float(v_string.strip())
+                avg = avg + value
+
+        print("Average spam confidence:", avg/count)
