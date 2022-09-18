ced615780263da5e537f6b5d23b292e492f01089
namredoc <102049850+namredoc@users.noreply.github.com>
2022-09-19 00:19:24 +0300

    Update EX_11.py

--- EX_11.py
+++ EX_11.py
@@ -2,26 +2,6 @@ import re
 
 # Solution 1
 
-# summ = 0
-# numm = 0
-# fh = open('regex_sum_1618683.txt')
-# for line in fh:
-# 	line = line.rstrip()
-# 	numbers = re.findall('[0-9]+', line)
-# 	# print(numbers)
-# 	for number in numbers:
-# 		# print(number)
-# 		numm = numm + 1
-# 		number = int(number)
-# 		summ = summ + number
-
-# print(summ, numm)
-
-# =========================
-
-# Solution 2
-
-# x = 0
 numlist = list()
 fh = open('test_text.txt')
 lines = fh.read()
@@ -29,5 +9,23 @@ numbers = re.findall('[0-9]+', lines)
 for number in numbers:
 	number = int(number)
 	numlist.append(number)
-	# x = x + number
 print(numbers, sum(numlist))
+
+
+# =========================
+
+# Solution 2
+
+
+summ = 0
+numm = 0
+fh = open('regex_sum_1618683.txt')
+for line in fh:
+	line = line.rstrip()
+	numbers = re.findall('[0-9]+', line)
+	for number in numbers:
+		numm = numm + 1
+		number = int(number)
+		summ = summ + number
+
+print(summ, numm)
